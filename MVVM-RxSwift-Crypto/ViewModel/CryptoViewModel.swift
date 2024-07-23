dafc89fb030a109f2fb9b74ec9a609b50abce79c
akaraoluk <abdurrahmankaraoluk@gmail.com>
2024-07-23 10:19:48 +0300

    [MVVM-RxSwift-Crypto][Version 1][Abdurrahman KARAOLUK][rxSwift-implemented]

--- MVVM-RxSwift-Crypto/ViewModel/CryptoViewModel.swift
+++ MVVM-RxSwift-Crypto/ViewModel/CryptoViewModel.swift
@@ -6,16 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class CryptoViewModel {
+    
+    let cryptos: PublishSubject<[Crypto]> = PublishSubject()
+    let error: PublishSubject<String> = PublishSubject()
+    let loading: PublishSubject<Bool> = PublishSubject()
+    
     func requestData() {
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         Webservice().downloadCurrencies(url: url) { result in
             switch result {
             case.success(let cryptos):
-                print(cryptos)
+                self.cryptos.onNext(cryptos)
             case .failure(let error):
-                print(error)
+                switch error {
+                case .parsingError:
+                    self.error.onNext("Parsing Error")
+                case .serverError:
+                    self.error.onNext("Server Error")
+                }
             }
         }
     }
